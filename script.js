c757b4ff882eca98e512b59ed3b48e3e1f02d9b6
rudhravyas <91895312+rudhravyas@users.noreply.github.com>
2026-01-19 17:02:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
 //complete this code
 class Person {
-	 constructor(name, age) {
+    constructor(name, age) {
         this._name = name;
         this._age = age;
     }
 
-	get name(){
-		return this._name;
-	}
-	set age(newAge) {
-    this._age = newAge;
-	}
+    get name() {
+        return this._name;
+    }
+
+    set age(newAge) {
+        this._age = newAge;
+    }
 }
 
 class Student extends Person {
-	teach() {
-    console.log(`${this.name} is teaching`);
-	}
+    study() {
+        console.log(`${this.name} is studying`);
+    }
 }
 
 class Teacher extends Person {
-	study() {
-        console.log("studying");
+    teach() {
+        console.log(`${this.name} is teaching`);
     }
 }
 
@@ -29,3 +30,4 @@ class Teacher extends Person {
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
+
